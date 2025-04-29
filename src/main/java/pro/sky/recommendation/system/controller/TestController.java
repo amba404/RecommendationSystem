8228ad34b6f827fc0ed12a57604bc8b66e5255c4
amba404 <amba.avg@gmail.com>
2025-04-30 00:17:00 +0900

    в тестовом контроллере изменил эндпоинт на корректный

--- src/main/java/pro/sky/recommendation/system/controller/TestController.java
+++ src/main/java/pro/sky/recommendation/system/controller/TestController.java
@@ -2,6 +2,7 @@ package pro.sky.recommendation.system.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pro.sky.recommendation.system.service.TestService;
 import pro.sky.recommendation.system.service.TestServiceImpl;
@@ -9,6 +10,7 @@ import pro.sky.recommendation.system.service.TestServiceImpl;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
     private final TestService service;
