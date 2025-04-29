d593d5760360e7b648a414a4b2100f8e7e8398a5
Наталья <korolkov.01@bk.ru>
2025-04-29 16:34:52 +0300

    Initial commit

--- src/main/java/pro/sky/recommendation/system/RecommendationSystemApplication.java
+++ src/main/java/pro/sky/recommendation/system/RecommendationSystemApplication.java
@@ -1,13 +1,15 @@
 package pro.sky.recommendation.system;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition
 @SpringBootApplication
 public class RecommendationSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecommendationSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationSystemApplication.class, args);
+    }
 
 }
